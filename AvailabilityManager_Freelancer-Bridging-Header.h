cfb703d95aa8016aa5a4f1465a4bfcb8045f6195
Tops <dipne.patel@tops-int.com>
2015-04-25 15:06:47 +0530

    Bridging

---
+++ AvailabilityManager_Freelancer-Bridging-Header.h
@@ -0,0 +1,24 @@
+//
+//  AvailabilityManager_Freelancer-Bridging-Header.h
+//  AvailabilityManager_Freelancer
+//
+//  Created by TopsTech on 03/03/2015.
+//  Copyright (c) 2015 tops. All rights reserved.
+//
+
+#ifndef AvailabilityManager_Freelancer_AvailabilityManager_Freelancer_Bridging_Header_h
+#define AvailabilityManager_Freelancer_AvailabilityManager_Freelancer_Bridging_Header_h
+
+#import "AFNetworking.h"
+#import "UIButton+AFNetworking.h"
+#import "UIImageView+AFNetworking.h"
+
+#import "MBProgressHUD.h"
+#import "AJNotificationView.h"
+#import "LocalizeHelper.h"
+#import "RDVCalendarView.h"
+#import "CollapseClick.h"
+#import "GiFHUD.h"
+#import "UITableView+DragLoad.h"
+
+#endif
